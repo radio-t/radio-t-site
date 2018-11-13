a8e9c592f5b6661db501cb32f460a9ff8ffeec0d
Umputun <umputun@gmail.com>
2018-11-13 13:57:12 -0600

    missing dir in updater path

--- updater/entrypoint.sh
+++ updater/entrypoint.sh
@@ -3,6 +3,6 @@ echo "$(date) start updater"
 
 while true
 do
-    ssh umputun@172.17.42.1 "/srv/site.hugo/updater.sh"
+    ssh umputun@172.17.42.1 "/srv/site.hugo/updater/updater.sh"
     sleep 10
 done
