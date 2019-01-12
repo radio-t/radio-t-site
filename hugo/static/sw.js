7ac5895e68e1556934e8de0823a15f45469aa260
Arthur G <lapteuh@gmail.com>
2019-01-12 19:56:49 +0500

    Add a dummy service-worker for modern browsers.

---
+++ hugo/static/sw.js
@@ -0,0 +1,7 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
