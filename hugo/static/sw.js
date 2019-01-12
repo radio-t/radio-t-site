c6842e149e5a4d38c8449ef1a56fc751521623e4
Arthur G <lapteuh@gmail.com>
2019-01-12 20:20:50 +0500

    Add caching images.

--- hugo/static/sw.js
+++ hugo/static/sw.js
@@ -1,7 +1,19 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+workbox.setConfig({ debug: false });
+
+// Caching Images by matching against a list of known extensions.
+// https://developers.google.com/web/tools/workbox/guides/common-recipes
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 100,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days        
+        purgeOnQuotaError: true, // Automatically cleanup if quota is exceeded.
+      }),
+    ],
+  })
+);
