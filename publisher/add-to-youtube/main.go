ca4c45a431648f50c1e20a658e4a373d88fa3e11
Arthur G <lapteuh@gmail.com>
2019-02-05 19:10:07 +0500

    configure logger

--- publisher/add-to-youtube/main.go
+++ publisher/add-to-youtube/main.go
@@ -1,6 +1,19 @@
 package main
 
-import "github.com/radio-t/radio-t-site/publisher/add-to-youtube/cmd"
+import (
+	"github.com/radio-t/radio-t-site/publisher/add-to-youtube/cmd"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		DisableTimestamp:       true,
+		ForceColors:            true,
+		DisableLevelTruncation: true,
+	})
+	log.SetLevel(log.InfoLevel)
+}
 
 func main() {
 	cmd.Execute()
