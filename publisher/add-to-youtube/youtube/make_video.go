a3a339a7e4a1b5f9f3c6e1cffc5301fa839d1488
Arthur G <lapteuh@gmail.com>
2019-02-12 02:23:21 +0500

    optimize video creation

--- publisher/add-to-youtube/youtube/make_video.go
+++ publisher/add-to-youtube/youtube/make_video.go
@@ -1,6 +1,7 @@
 package youtube
 
 import (
+	"io/ioutil"
 	"os"
 	"os/exec"
 
@@ -11,12 +12,28 @@ import (
 func makeVideo(audioPath, videoPath string) error {
 	log.Info("Start making a video")
 
-	exec := exec.Command("ffmpeg", "-loop", "1", "-i", "assets/cover.webp", "-i", audioPath, "-c:v", "libx264", "-r", "15", "-c:a", "copy", "-shortest", "-y", "-pix_fmt", "yuv420p", videoPath)
+	f, err := ioutil.TempFile("", "intermedia_*.mp4")
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
+		if err := os.Remove(f.Name()); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-	exec.Stdout = os.Stdout
-	exec.Stderr = os.Stderr
+	createIntermediaVideoCommad := exec.Command("ffmpeg", "-y", "-loop", "1", "-i", "assets/cover.webp", "-c:v", "libx264", "-r", "15", "-pix_fmt", "yuv420p", "-t", "60", f.Name())
 
-	if err := exec.Run(); err != nil {
+	if err := createIntermediaVideoCommad.Run(); err != nil {
+		return errors.Wrap(err, "Error creating an intermedia file")
+	}
+
+	createVideoCommand := exec.Command("ffmpeg", "-y", "-stream_loop", "1", "-i", f.Name(), "-i", audioPath, "-c:v", "copy", "-c:a", "copy", "-shortest", videoPath)
+
+	if err := createVideoCommand.Run(); err != nil {
 		return errors.Wrap(err, "Error executing a ffmpeg")
 	}
 
