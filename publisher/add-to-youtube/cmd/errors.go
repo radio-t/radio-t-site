08d3e43e1095c3efab2a5894842515357d3ed9af
Arthur G <lapteuh@gmail.com>
2019-02-12 04:13:34 +0500

    refactoring

--- publisher/add-to-youtube/cmd/errors.go
+++ publisher/add-to-youtube/cmd/errors.go
@@ -3,9 +3,9 @@ package cmd
 import "github.com/pkg/errors"
 
 func errSiteAPIRequest(err error) error {
-	return errors.Wrap(err, "Error making site api request, got: %v")
+	return errors.Wrap(err, "Error making site api request")
 }
 
 func errJSONUnmarshal(err error) error {
-	return errors.Wrap(err, "Error json unmarshaling, got: %v")
+	return errors.Wrap(err, "Error json unmarshaling")
 }
