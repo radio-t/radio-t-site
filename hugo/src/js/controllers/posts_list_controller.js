0e8cb0d517c225e5c788123a7740df4aad77de91
Konstantin L <sw.double@gmail.com>
2019-05-04 18:56:26 +0200

    suggest topic button visibility

---
+++ hugo/src/js/controllers/posts_list_controller.js
@@ -0,0 +1,28 @@
+import Controller from '../base_controller';
+
+export default class extends Controller {
+  initialize() {
+    super.initialize();
+
+    this.showSuggestTopicButton();
+  }
+
+  /**
+   * first prep that goes before any podcast
+   */
+  showSuggestTopicButton() {
+    // Only first page
+    if (this.data.get('page-number') !== '1') return;
+
+    for (let post of this.element.children) {
+      if (post.classList.contains('posts-list-item-category-podcast')) {
+        break;
+      }
+      if (post.classList.contains('posts-list-item-category-prep')) {
+        const btn = post.querySelector('.btn-suggest-topic');
+        if (btn) btn.classList.remove('d-none');
+        break;
+      }
+    }
+  }
+}
