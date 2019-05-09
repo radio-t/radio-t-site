247c6578501b347b114e7777300e326c72cda31c
Konstantin L <sw.double@gmail.com>
2019-05-10 01:23:25 +0200

    favicons

--- hugo/gulpfile.js
+++ hugo/gulpfile.js
@@ -1,15 +1,15 @@
-const gulp = require('gulp')
-const favicons = require('gulp-favicons')
+const gulp = require('gulp');
+const favicons = require('gulp-favicons');
 
 gulp.task('favicons', function () {
   return gulp.src('./favicon.png')
     .pipe(favicons({
-      appName: '',
-      appDescription: '',
+      appName: 'Радио-Т Подкаст',
+      appDescription: 'Подкаст выходного дня - импровизации на темы высоких технологий',
       // developerName: '',
       // developerURL: '',
-      background: '#fff',
-      path: '/favicons/',
+      background: 'transparent',
+      path: '/',
       // url: '',
       // display: 'standalone',
       // orientation: 'portrait',
@@ -17,10 +17,30 @@ gulp.task('favicons', function () {
       // version: 1.0,
       logging: true,
       online: false,
-      html: '../favicons.html',
+      html: '../layouts/partials/favicons.html',
       pipeHTML: true,
       replace: true,
+      icons: {
+        // Platform Options:
+        // - offset - offset in percentage
+        // - background:
+        //   * false - use default
+        //   * true - force use default, e.g. set background for Android icons
+        //   * color - set background for the specified icons
+        //   * mask - apply mask in order to create circle icon (applied by default for firefox). `boolean`
+        //   * overlayGlow - apply glow effect after mask has been applied (applied by default for firefox). `boolean`
+        //   * overlayShadow - apply drop shadow after mask has been applied .`boolean`
+        //
+        favicons: true,              // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+        android: false,              // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+        appleIcon: false,            // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+        appleStartup: false,         // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+        coast: false,                // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+        firefox: false,              // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+        windows: false,              // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+        yandex: false,               // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+      },
     }))
     .on('error', require('fancy-log'))
-    .pipe(gulp.dest('./favicons/'))
-})
+    .pipe(gulp.dest('./static/'));
+});
