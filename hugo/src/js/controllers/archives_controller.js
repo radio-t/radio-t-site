5356f41694ba9dc345e56f47c75ce9bb9956533d
Konstantin L <sw.double@gmail.com>
2019-05-10 00:34:48 +0200

    archives

--- hugo/src/js/controllers/archives_controller.js
+++ hugo/src/js/controllers/archives_controller.js
@@ -12,6 +12,10 @@ export default class extends Controller {
 
   initialize() {
     super.initialize();
+
+    if (this.data.get('initialized')) return;
+    this.data.set('initialized', '1');
+
     this.ranges = this.listTargets.map((list) => {
       const latest = find(list.children, (post) => post.querySelector('.podcast-title-number'));
       const earliest = findLast(list.children, (post) => post.querySelector('.podcast-title-number'));
@@ -29,26 +33,6 @@ export default class extends Controller {
       easing: 'easeOutCubic',
       updateURL: false,
       popstate: false,
-    });
-
-    this.rangeTargets.forEach((target, index) => {
-      target.style.transitionDelay = `0ms`;
-      target.style.opacity = 0;
-      target.offsetHeight;
-
-      target.style.transitionDelay = `${150 + index * 50}ms`;
-      target.style.transitionDuration = `50ms`;
-      target.style.transitionProperty = 'opacity';
-      target.style.opacity = 1;
-    });
-  }
-
-  disconnect() {
-    super.disconnect();
-    this.rangeTargets.forEach((target, index) => {
-      target.style.transitionDelay = `0ms`;
-      target.style.opacity = 0;
-      target.offsetHeight;
     });
   }
 }
