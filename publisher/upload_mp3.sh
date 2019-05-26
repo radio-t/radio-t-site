f3dbc048d36923876dc9634ee44127ee26364bb5
Eugene <eugene@umputun.com>
2019-05-26 16:46:20 -0500

    set 644 after upload to archives

--- publisher/upload_mp3.sh
+++ publisher/upload_mp3.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-currdir=`dirname $0`
+currdir=$(dirname $0)
 cd ${currdir}
 echo "current dir=$currdir"
 
 export LANG="en_US.UTF-8"
-fname=`basename $1`
+fname=$(basename $1)
 
 lftp="/usr/local/bin/lftp"
 notif="/usr/local/bin/terminal-notifier"
 
-episode=`echo $1 | sed -n 's/.*rt_podcast\(.*\)\.mp3/\1/p'`
+episode=$(echo $1 | sed -n 's/.*rt_podcast\(.*\)\.mp3/\1/p')
 image="../hugo/static/images/covers/cover.png"
 ${notif} -title PodPrc -message "Radio-T detected #${episode}"
-utils/eyeD3.exec -v --remove-all --set-encoding=utf8 --album="Радио-Т" --add-image=${image}:FRONT_COVER: --artist="Umputun, Bobuk, Gray, Ksenks" --track=${episode} --title="Радио-Т ${episode}" --year=$(date +%Y)  --genre="Podcast" $1
+utils/eyeD3.exec -v --remove-all --set-encoding=utf8 --album="Радио-Т" --add-image=${image}:FRONT_COVER: --artist="Umputun, Bobuk, Gray, Ksenks" --track=${episode} --title="Радио-Т ${episode}" --year=$(date +%Y) --genre="Podcast" $1
 ${notif} -title PodPrc -message "Radio-T tagged"
 
 cd ..
@@ -34,6 +34,7 @@ scp -P 2222 $1 umputun@archives.umputun.com:/data/archive.rucast.net/radio-t/med
 
 echo "upload to archive site"
 scp $1 umputun@master.radio-t.com:/data/archive/radio-t/media/${fname}
+ssh umputun@master.radio-t.com "chmod 644 /data/archive/radio-t/media/${fname}"
 
 echo "all done for $fname"
 ${notif} -title PodPrc -message "all done for $fname"
