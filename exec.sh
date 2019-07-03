8498fc871c7b091ab50584457ec66aabec767ede
Konstantin L <sw.double@gmail.com>
2019-07-04 02:37:19 +0300

    use hugo minify flag

--- exec.sh
+++ exec.sh
@@ -4,5 +4,5 @@ echo " === copy frontend ==="
 cp -rf /app/static/build /srv/hugo/static/
 cp -rf /app/data/manifest.json /srv/hugo/data/
 echo " === generate pages ==="
-hugo
+hugo --minify
 /srv/hugo/generate_rss.py
