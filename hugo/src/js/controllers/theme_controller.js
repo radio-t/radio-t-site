7d51036d0be2db16a140dac2c23e779f3ae856e2
Denis Panfilov <flaticols@outlook.com>
2019-07-12 22:48:51 +0300

    Added auto-dark mode.

--- hugo/src/js/controllers/theme_controller.js
+++ hugo/src/js/controllers/theme_controller.js
@@ -1,49 +1,13 @@
 import Controller from '../base_controller';
 
-const titleAttribute = 'data-theme';
-let theme = window.RADIOT_THEME;
+import { setTheme } from "../theme-utils";
 
 export default class extends Controller {
   toggle() {
     this.toggleTheme();
   }
 
-  getStylesheets() {
-    const styles = document.querySelectorAll(`link[${titleAttribute}][rel~="stylesheet"]`);
-    return [].slice.call(styles);
-  }
-
-  enableStylesheet(link, enable) {
-    // link.media = enable ? '' : 'none';
-    if (enable) {
-      link.media = '';
-    } else {
-      // Delay disabling to prevent FOUC
-      setTimeout(() => link.media = 'none', 100);
-    }
-  }
-
-  setTheme(theme) {
-    try {
-      localStorage.setItem('theme', theme);
-    } catch (e) {
-      //
-    }
-
-    this.getStylesheets().forEach((link) => {
-      this.enableStylesheet(link, link.getAttribute(titleAttribute) === theme);
-    });
-
-    window.RADIOT_THEME = theme;
-
-    const event = document.createEvent('Events');
-    event.initEvent('theme:change', true);
-    document.dispatchEvent(event);
-  }
-
   toggleTheme() {
-    theme = 'dark' === theme ? 'light' : 'dark';
-
-    this.setTheme(theme);
+    setTheme('dark' === window.RADIOT_THEME ? 'light' : 'dark');
   }
 }
