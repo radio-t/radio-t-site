6ca64ac7c48bf54541b9e5b404d4f4eed26adc5f
q210 <q210.frk@gmail.com>
2020-01-19 00:30:50 +0300

    Rewrite `make_new_episode.sh` using python

--- publisher/tasks/episode_info.py
+++ publisher/tasks/episode_info.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Union
 from urllib.parse import urljoin
 
 import requests
@@ -11,18 +12,23 @@ def print_next_episode_number(c):
     """
     Print to stdout next podcast episode number parsed from https://radio-t.com/
     """
-    headers = {"User-Agent": c.http.user_agent}
-    resp = requests.get(c.http.site_url, headers=headers, timeout=c.http.timeout)
+    last_episode_num = get_last_podcast_number(c.http.site_url, c.http.user_agent, c.http.timeout)
+    if not last_episode_num:
+        print("Error:", f"Last podcast episode page not found", file=sys.stderr)
+        sys.exit(1)
+
+    print(last_episode_num + 1)
+
+
+def get_last_podcast_number(site_url: str, user_agent: str, timeout: int = 30) -> Union[int, None]:
+    headers = {"User-Agent": user_agent}
+    resp = requests.get(site_url, headers=headers, timeout=timeout)
     soup = BeautifulSoup(resp.content, "html.parser")
 
     for item in soup.find_all("h2", class_="number-title"):
         if "podcast-" in item.a["href"]:
             last_podcast_num = int(item.a["href"].strip("/").rsplit("podcast-", 1)[-1])
-            print(last_podcast_num + 1)
-            return
-
-    print("Error:", f"Last podcast episode page not found", file=sys.stderr)
-    sys.exit(1)
+            return last_podcast_num
 
 
 @task
@@ -30,14 +36,19 @@ def print_last_rt_link(c):
     """
     Print to stdout last podcast episode link parsed from https://radio-t.com/
     """
-    headers = {"User-Agent": c.http.user_agent}
-    resp = requests.get(c.http.site_url, headers=headers, timeout=c.http.timeout)
+    last_podcast_page_link = get_last_episode_link(c.http.site_url, c.http.user_agent, c.http.timeout)
+    if not last_podcast_page_link:
+        print("Error:", f"Link to last podcast episode page not found", file=sys.stderr)
+        sys.exit(1)
+
+    print(last_podcast_page_link)
+
+
+def get_last_episode_link(site_url: str, user_agent: str, timeout: int = 30) -> Union[str, None]:
+    headers = {"User-Agent": user_agent}
+    resp = requests.get(site_url, headers=headers, timeout=timeout)
     soup = BeautifulSoup(resp.content, "html.parser")
 
     for item in soup.find_all("h2", class_="number-title"):
         if "podcast-" in item.a["href"]:
-            print(urljoin(c.http.site_url, item.a["href"]))
-            return
-
-    print("Error:", f"Link to last podcast episode page not found", file=sys.stderr)
-    sys.exit(1)
+            return urljoin(site_url, item.a["href"])
--- publisher/tasks/hugo_generation.py
+++ publisher/tasks/hugo_generation.py
@@ -1,4 +1,14 @@
+import os.path
+import sys
+from datetime import datetime as dt
+from string import Template
+
+import requests
 from invoke import task
+
+from .episode_info import get_last_episode_link, get_last_podcast_number
+
+TEMPLATES_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "templates")
 
 
 @task
@@ -6,7 +16,37 @@ def new_episode(c):
     """
     Generate new `./content/posts/podcast-$(next episode number).md` file
     """
-    c.run("./scripts/make_new_episode.sh")
+    # get new episode number from https://radio-t.com/
+    last_episode_num = get_last_podcast_number(c.http.site_url, c.http.user_agent, c.http.timeout)
+    if not last_episode_num:
+        print("Error:", f"Last podcast episode page not found", file=sys.stderr)
+        sys.exit(1)
+
+    next_episode_num = last_episode_num + 1
+    print(f"New post number: {next_episode_num}")
+
+    # get template for new episode post
+    new_file_path_relative = Template(c.hugo.episode_post).substitute(episode_num=next_episode_num).lstrip("/")
+    new_file_path = os.path.join("/srv", new_file_path_relative)
+    with open(os.path.join(TEMPLATES_DIR, "new_episode_post.tmpl"), "r") as f:
+        new_episode_template = Template(f.read())
+
+    # get themes from https://news.radio-t.com API
+    headers = {"User-Agent": c.http.user_agent}
+    news_api_resp = requests.get(c.http.themes_url, headers=headers, timeout=c.http.timeout)
+
+    # write new episode post to hugo posts directory
+    with open(new_file_path, "w", encoding="utf-8") as f:
+        f.write(
+            new_episode_template.substitute(
+                episode_num=next_episode_num,
+                timestamp=dt.now().strftime("%Y-%m-%dT%H:%M:%S"),
+                themes=news_api_resp.text.strip(),
+            )
+        )
+
+    print("New episode post generated. File:")
+    print(new_file_path_relative)
 
 
 @task
