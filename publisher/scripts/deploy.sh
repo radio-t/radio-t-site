0e09cc58da0b2ff8f5b6d08c335cd05c5497138b
Umputun <umputun@gmail.com>
2020-02-01 18:52:30 -0600

    fix admin name

--- publisher/scripts/deploy.sh
+++ publisher/scripts/deploy.sh
@@ -13,7 +13,7 @@ git commit -m "auto episode after $num_before" && git push
 ssh umputun@master.radio-t.com "cd /srv/site.hugo && git pull && docker-compose run --rm hugo"
 
 cd publisher
-ssh umputun@master.radio-t.com "docker exec -i super-bot /srv/telegram-rt-bot --super=Umputun --super=bobuk --super=ksenks --super=grayru --dbg --export-num=$num_before --export-path=/srv/html"
+ssh umputun@master.radio-t.com "docker exec -i super-bot /srv/telegram-rt-bot --super=umputun --super=bobuk --super=ksenks --super=grayru --dbg --export-num=$num_before --export-path=/srv/html"
 num_after=`invoke print-next-episode-number 2>/dev/null`
 
 if [[ $num_before != $num_after ]]
