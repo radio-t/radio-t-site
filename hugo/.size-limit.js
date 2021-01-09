b094fbb32df0cbebe66a62adda054705303478c5
Pavel Mineev <pavel@mineev.me>
2021-01-10 02:01:00 +0300

    add size limit

---
+++ hugo/.size-limit.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    path: 'static/build/app.js',
+    limit: '14.33 KB'
+  },
+  {
+    path: 'static/build/manifest.js',
+    limit: '717 B',
+  },
+  {
+    path: 'static/build/modernizr-bundle.js',
+    limit: '1.98 KB'
+  },
+  {
+    path: 'static/build/vendor.js',
+    vendor: '424.46 KB',
+  },
+];
