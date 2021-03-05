d7b03689f5defe84085c3da401aea3cbcdf1afe8
adfslslddkds <79110543+adfslslddkds@users.noreply.github.com>
2021-03-05 21:27:23 +0300

    JS Lins fix.
    Add fallbackCopyTextToClipboard function for copyTextToClipboard.

--- hugo/src/js/utils.js
+++ hugo/src/js/utils.js
@@ -51,6 +51,30 @@ export function getTextSnippet(html) {
 }
 
 //https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    alert('Временная метка скопирована в буфер обмена', msg, '\nTO DO: сделать красивое оповещение');
+  } catch (err) {
+    alert('ОШИБКА: невозможно скопировать временную метку в буфер обмена    ', err, "\nTO DO: сделать красивое оповещение");
+  }
+
+  document.body.removeChild(textArea);
+}
+
 export function copyTextToClipboard(text) {
   if (!navigator.clipboard) {
     fallbackCopyTextToClipboard(text);
