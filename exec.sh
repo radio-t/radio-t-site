aeec21ba45e4172b1a9b45d71196c2386e9b763c
Dmitry Verkhoturov <paskal.07@gmail.com>
2024-11-01 20:54:12 +0100

    Disable minification during Hugo build for diffs
    - Add `DO_NOT_MINIFY` and `DO_NOT_MINIFY_HUGO` flags to Docker builds to prevent Hugo minification
    - Copy `manifest.json` files from both old and new builds for comparison after build instead of checking them out from the code (where they might be outdated)
    - Fix markdown in `changes.md` generation
    - Improve minify env variable name

--- exec.sh
+++ exec.sh
@@ -4,7 +4,7 @@ echo " === copy frontend ==="
 cp -rf /app/static/build /srv/hugo/static/
 cp -rf /app/data/manifest.json /srv/hugo/data/
 echo " === generate pages ==="
-if [ -z "$DO_NOT_MINIMIFY" ]; then
+if [ -z "$DO_NOT_MINIFY_HUGO" ]; then
   echo " === build with minify ==="
   hugo --minify
 else
