6a5c7c7b05641a1bb24f0898e2542dae90ad146a
Dmitry Verkhoturov <paskal.07@gmail.com>
2024-11-01 20:00:42 +0100

    Remove css and js ids from the diff, add unminified build option
    With this, HTML changes will look cleaner, and changes of JS and CSS
    files which alter hashes to retrieve them in the head of the page
    wouldn't generate megabytes of diff.

--- exec.sh
+++ exec.sh
@@ -4,5 +4,11 @@ echo " === copy frontend ==="
 cp -rf /app/static/build /srv/hugo/static/
 cp -rf /app/data/manifest.json /srv/hugo/data/
 echo " === generate pages ==="
-hugo --minify
+if [ -z "$DO_NOT_MINIMIFY" ]; then
+  echo " === build with minify ==="
+  hugo --minify
+else
+  echo " === build without minify ==="
+  hugo
+fi
 /usr/local/bin/rss_generator
