15fe49ecfe3958d9c3b410d0069dae27223487c8
Dmitry Verkhoturov <paskal.07@gmail.com>
2025-03-09 14:44:37 +0100

    Get rid of get.options as we use only params

--- hugo/src/js/http-get.js
+++ hugo/src/js/http-get.js
@@ -2,27 +2,12 @@ const cache = new Map();
 const throttleMap = new Map();
 
 /**
- * Fetch wrapper with basic caching and throttling
+ * Fetch JSON from URL with caching and throttling
  * @param {string} url - URL to fetch
- * @param {Object} options - Fetch options
  * @returns {Promise<any>} - Promise with parsed JSON response
  */
-async function fetchWithCache(url, options = {}) {
-  // Handle params if present
-  if (options.params) {
-    const urlObj = new URL(url);
-    Object.entries(options.params).forEach(([key, value]) => {
-      urlObj.searchParams.append(key, value);
-    });
-    url = urlObj.toString();
-    // Remove params from options to avoid duplication
-    delete options.params;
-  }
-
-  // sort options to make sure the cache key is consistent
-  const cacheKey = url + (Object.keys(options).length ? `-${JSON.stringify(Object.keys(options).sort().reduce((acc, key) => ({
-    ...acc, [key]: options[key]
-  }), {}))}` : '');
+export async function fetchJSON(url) {
+  const cacheKey = url;
 
   if (throttleMap.has(cacheKey)) {
     return throttleMap.get(cacheKey);
@@ -34,7 +19,7 @@ async function fetchWithCache(url, options = {}) {
 
   const promise = (async () => {
     try {
-      const response = await fetch(url, options);
+      const response = await fetch(url, { method: 'GET' });
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -54,12 +39,3 @@ async function fetchWithCache(url, options = {}) {
 
   return promise;
 }
-
-export default {
-  get: async (url, options = {}) => {
-    const data = await fetchWithCache(url, {
-      method: 'GET', ...options
-    });
-    return {data};
-  }
-};
